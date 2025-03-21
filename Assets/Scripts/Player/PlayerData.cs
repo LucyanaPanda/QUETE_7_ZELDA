735b60a11436cf26f6bbc24fe0e5d6ca7f971958
LucyanaPanda <lucie.nguyenpham@gmail.com>
2025-03-21 15:21:54 +0100

    Save Player Data system done

---
+++ Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,14 @@
+
+using NUnit.Framework;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    public Creature creatureData;
+    public float health, minHealth, maxHealth;
+    public float attackTimer, attackMaxTimer;
+    public float attack, minAttack, maxAttack;
+    public float defense, minDefense, maxDefense;
+    public float speed, minSpeed, maxSpeed;
+}
--- Assets/Scripts/UI/Settings/SettingsManager.cs
+++ Assets/Scripts/UI/Settings/SettingsManager.cs
@@ -3,40 +3,43 @@ using UnityEngine;
 public class SettingsManager : MonoBehaviour
 {
     [Header("Settings")]
-    [SerializeField] private GameObject _settingsCanvas;
+    [SerializeField] private GameObject _settingsPanel;
     [SerializeField] private MenuButton _settingsButton;
     [SerializeField] private MenuButton _quitSettings;
 
     [Header("Audio")]
-    [SerializeField] private GameObject _audio;
+    [SerializeField] private GameObject _audioPanel;
     [SerializeField] private MenuButton _audioButton;
 
     [Header("Display")]
-    [SerializeField] private GameObject _display;
+    [SerializeField] private GameObject _displayPanel;
     [SerializeField] private MenuButton _displayButton;
 
     private void Start()
     {
+        //Settings;
         _settingsButton.OnButtonPressed.AddListener(() => ShowHideSettings(true));
         _quitSettings.OnButtonPressed.AddListener(() => ShowHideSettings(false));
 
+        //Audio
         _audioButton.OnButtonPressed.AddListener(() => ShowHideAudio(true));
+        //Display
         _displayButton.OnButtonPressed.AddListener(() => ShowHideDisplay(true));
     }
 
     public void ShowHideSettings(bool settings)
     {
-        _settingsCanvas.SetActive(settings);
+        _settingsPanel.SetActive(settings);
     }
 
     public void ShowHideAudio(bool audio)
     {
-        _audio.SetActive(audio);
-        _display.SetActive(!audio);
+        _audioPanel.SetActive(audio);
+        _displayPanel.SetActive(!audio);
     }
     public void ShowHideDisplay(bool display)
     {
-        _display.SetActive(display);
-        _audio.SetActive(!display);
+        _displayPanel.SetActive(display);
+        _audioPanel.SetActive(!display);
     }
 }
